94e3ac474403512051e622c2515c7192fc356fc2
ed-leonardo <eduardo_alves96@hotmail.com>
2020-08-09 22:35:51 -0300

    altered: bankTitle services

--- src/services/BankTitles.ts
+++ src/services/BankTitles.ts
@@ -6,39 +6,44 @@ export default function bankTitle(line: string) {
     
   const firstField = line.substr(0, 9)
   let digit = parseInt(line[9])
-  let prefix = firstField.slice(0, 4)
+  const prefix = firstField.slice(0, 4)
   let suffix = firstField.slice(4)
   isValid = verifyModule10(firstField, digit)
 
   if (isValid) {
-      const secondField = line.substr(10, 10);
-      digit = parseInt(line[20])
-      suffix += secondField;
-      isValid = verifyModule10(secondField, digit);
+    const secondField = line.substr(10, 10);
+    digit = parseInt(line[20])
+    suffix += secondField;
+    isValid = verifyModule10(secondField, digit);
 	}
 	
   if (isValid) {
-      const thirdField = line.substr(21, 10);
-      digit = parseInt(line[31])
-      suffix += thirdField;
-      isValid = verifyModule10(thirdField, digit);
+    const thirdField = line.substr(21, 10);
+    digit = parseInt(line[31])
+    suffix += thirdField;
+    isValid = verifyModule10(thirdField, digit);
 	}
 	
   let dueDateCoefficient=0
   let fifthField, value
-	let dueDate = new Date(2030, 1, 22)
+	let dueDate = new Date(2025, 1, 22)
 	
   if (isValid) {
     fifthField = line.substr(33);
     const today = new Date();
     
-    if (today >= dueDate)
-        dueDate.setDate(dueDate.getDate()-1000);
-    else
-        dueDate.setDate(dueDate.getDate()-10000);
+    if (today >= dueDate) {
+      dueDate.setDate(dueDate.getDate()-1000);
+    }
+    else {
+      dueDate.setDate(dueDate.getDate()-10000);
+    }
+
     dueDateCoefficient = parseInt(fifthField.slice(0, 4));
-    if (dueDateCoefficient < 1000)
-        value = parseFloat(fifthField.slice(0, -2) + '.' + fifthField.slice(-2));
+
+    if (dueDateCoefficient < 1000) {
+      value = parseFloat(fifthField.slice(0, -2) + '.' + fifthField.slice(-2));
+    }
     else {
         dueDate.setDate(dueDate.getDate() + dueDateCoefficient);
         value = parseFloat(fifthField.slice(4, -2) + '.' + fifthField.slice(-2));
@@ -52,10 +57,15 @@ export default function bankTitle(line: string) {
   if (isValid) {
     const codeBar = prefix + digit + fifthField + suffix;
     if (dueDateCoefficient < 1000) {
-			return { Value: value, codeBar: codeBar };
+			return { 
+        Valid: true,
+        Value: value, 
+        codeBar: codeBar 
+      }
 		}
     else {
 			return {
+        Valid: true,
         Value: value,
         'Due Date': dueDate.toLocaleString('pt-br',
         	{
